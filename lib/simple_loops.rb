9cd1e34a4ba7f6acdb0f8520a9c63674cc25e6b6
Marc Ferraro <marc.ferraro1@gmail.com>
2020-10-28 20:32:27 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -7,22 +7,29 @@ def loop_message_five_times(string)
   end
 end
 
-#def loop_message_n_times(string, total)
+=begin
+def loop_message_n_times(string, total)
  counter = 0
 
- # while counter <= total do
- #   puts string
- #   counter += 1
-#  end
-#end
+ while counter <= total do
+   puts string
+   counter += 1
+  end
+end
+=end
 
 def loop_message_n_times(string, counter)
   
   counter.times do
     puts string
   end
-def output_array
   
+def output_array(array)
+  counter = 0
+  while arrary[counter] do
+    puts array[counter]
+    counter += 1
+  end
 end
 
 def return_string_array
